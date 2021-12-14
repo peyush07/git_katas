deafebcdb33e123047c0153a9892c99ec065a0cb
Peyush <peyush707@gmail.com>
2021-12-14 17:51:41 +0530

    Created mergeSort.cpp in master branch

---
+++ mergeSort.cpp
@@ -0,0 +1,27 @@
+// C++ program for Merge Sort
+#include <iostream>
+using namespace std;
+
+// UTILITY FUNCTIONS
+// Function to print an array
+void printArray(int A[], int size)
+{
+    for (auto i = 0; i < size; i++)
+        cout << A[i] << " ";
+}
+
+// Driver code
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    auto arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Given array is \n";
+    printArray(arr, arr_size);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    cout << "\nSorted array is \n";
+    printArray(arr, arr_size);
+    return 0;
+}
